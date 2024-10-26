c0bfbd12ab32899e45c89f3699140f36895a4316
Enes Oğuz <githubenes10@outlook.com>
2024-10-26 13:42:00 +0300

    Component

---
+++ 02-component/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Component() {
+  return (
+    <div>
+        <div>
+            <h1>COMPONENT</h1>
+            <p>Qui dolore duis Lorem Lorem anim qui enim et enim sit do enim. </p>
+        </div>
+    </div>
+  )
+}
+
+export default Component
